a11c3d9962961e554894ddbdbcbaf6ee38e7fe95
wenzaidihu <750625920@qq.com>
2019-12-01 18:43:39 +0800

    初始化项目

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>
+      HelloWorld
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
