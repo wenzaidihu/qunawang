de790d1c88f9a6193451177ff014ea7eba25bd35
wenzaidihu <750625920@qq.com>
2019-12-22 22:02:08 +0800

    修改bug

--- src/common/gallary/Gallary.vue
+++ src/common/gallary/Gallary.vue
@@ -3,7 +3,7 @@
     <div class="wrapper">
       <swiper :options="swiperOptions">
         <swiper-slide v-for="(item, index) in imgs" :key="index">
-          <img class="gallary-img" src="item"/>
+          <img class="gallary-img" :src="item"/>
         </swiper-slide>
           <div class="swiper-pagination" slot="pagination"></div>
       </swiper>
