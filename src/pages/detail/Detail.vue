66adf2f03dce55d54487eeba72721f79193d88bd
wenzaidihu <750625920@qq.com>
2019-12-22 18:07:36 +0800

    实现Header渐隐渐显效果，使用递归组件实现详情页列表

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,18 +1,48 @@
 <template>
   <div>
     <DetailBanner></DetailBanner>
+    <DetailHeader></DetailHeader>
+    <DetailList :list="list"></DetailList>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      // sightName: '',
+      // bannerImg: '',
+      // gallaryImgs: [],
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票'
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+  .content
+    height: 50rem
 </style>
